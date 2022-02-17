6b5b7b50b260b994d6eebb6427f5f86bcc6a3d0c
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-02-17 10:16:22 +0530

    feat : added increase and decrease button

---
+++ JS_file/script_cart.js
@@ -0,0 +1,11 @@
+const btn_increase = document.querySelector("#qunt_increase");
+const btn_decrease = document.querySelector("#qunt_decrease");
+const input_num = document.querySelector(".input_num");
+btn_increase.addEventListener("click", () => {
+    var input_num_count = Number(input_num.innerText);
+    input_num.innerText = input_num_count + 1
+})
+btn_decrease.addEventListener("click", () => {
+    var input_num_count = Number(input_num.innerText);
+    input_num.innerText = input_num_count - 1
+})
