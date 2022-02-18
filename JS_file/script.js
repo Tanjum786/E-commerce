d8b865669865809a39ba1e486e3be152d5008bb2
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-02-18 16:06:06 +0530

    refactor : created main css file
    removed common property and used common class for that

--- JS_file/script.js
+++ JS_file/script.js
@@ -1,11 +1,10 @@
-    const userPassword = document.querySelector("#userPassword");
-    const showpassword = document.querySelector("#check_btn");
-    showpassword.addEventListener("click", () => {
-        if (userPassword.type === "password") {
-            userPassword.type = "text"
-        } else {
-            userPassword.type = "password"
-        }
-    });
+const userPassword = document.querySelector("#userPassword");
+const showpassword = document.querySelector("#check_btn");
+showpassword.addEventListener("click", () => {
+    if (userPassword.type === "password") {
+        userPassword.type = "text"
+    } else {
+        userPassword.type = "password"
+    }
+});
 
-    
